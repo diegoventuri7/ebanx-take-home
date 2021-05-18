e4dc7de1cc64df959ee35687ef671d5497d3db27
Diego Venturi <diego.venturi@objective.com.br>
2021-05-18 19:12:34 -0300

    test: improved integration tests

--- test/integration/api-endpoints.test.js
+++ test/integration/api-endpoints.test.js
@@ -22,8 +22,7 @@ describe('API-endpoints', function () {
       .end(function (err, res) {
         expect(err).is.equal(null)
         expect(res).to.have.status(200)
-        expect(res.text).to.be.equal('pong')
-        expect(res.body).to.be.a('object').that.is.empty
+        expect(res.body).to.be.equal('pong')
         done()
       })
   })
@@ -34,8 +33,139 @@ describe('API-endpoints', function () {
       .end(function (err, res) {
         expect(err).is.equal(null)
         expect(res).to.have.status(200)
-        expect(res.text).to.be.equal('OK')
-        expect(res.body).to.be.a('object').that.is.empty
+        expect(res.body).to.be.equal('OK')
+        done()
+      })
+  })
+
+  it('Bad day: Get balance for non-existing account', function (done) {
+    const query = { account_id: '1234' }
+
+    chai.request(this.app)
+      .get('/balance')
+      .query(query)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(404)
+        expect(res.body).to.be.equal(0)
+        done()
+      })
+  })
+
+  it('Happy day: Create account with initial balance', function (done) {
+    const body = { type: 'deposit', destination: '100', amount: 10 }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(201)
+        expect(res.body).to.be.a('object')
+
+        expect(res.body.destination).to.be.a('object')
+        expect(res.body.destination.id).to.be.equal('100')
+        expect(res.body.destination.balance).to.be.equal(10)
+        done()
+      })
+  })
+
+  it('Happy day: Deposit into existing account', function (done) {
+    const body = { type: 'deposit', destination: '100', amount: 10 }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(201)
+        expect(res.body).to.be.a('object')
+
+        expect(res.body.destination).to.be.a('object')
+        expect(res.body.destination.id).to.be.equal('100')
+        expect(res.body.destination.balance).to.be.equal(20)
+        done()
+      })
+  })
+
+  it('Happy day: Get balance for existing account', function (done) {
+    const query = { account_id: '100' }
+
+    chai.request(this.app)
+      .get('/balance')
+      .query(query)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.equal(20)
+        done()
+      })
+  })
+
+  it('Bad day: Withdraw from non-existing account', function (done) {
+    const body = { type: 'withdraw', origin: '200', amount: 10 }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(404)
+        expect(res.body).to.be.equal(0)
+        done()
+      })
+  })
+
+  it('Happy day: Withdraw from existing account', function (done) {
+    const body = { type: 'withdraw', origin: '100', amount: 5 }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(201)
+        expect(res.body).to.be.a('object')
+
+        expect(res.body.origin).to.be.a('object')
+        expect(res.body.origin.id).to.be.equal('100')
+        expect(res.body.origin.balance).to.be.equal(15)
+        done()
+      })
+  })
+
+  it('Happy day: Transfer from existing account', function (done) {
+    const body = { type: 'transfer', origin: '100', amount: 15, destination: '300' }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(201)
+        expect(res.body).to.be.a('object')
+
+        expect(res.body.origin).to.be.a('object')
+        expect(res.body.origin.id).to.be.equal('100')
+        expect(res.body.origin.balance).to.be.equal(0)
+
+        expect(res.body.destination).to.be.a('object')
+        expect(res.body.destination.id).to.be.equal('300')
+        expect(res.body.destination.balance).to.be.equal(15)
+        done()
+      })
+  })
+
+  it('Bad day: Transfer from non-existing account', function (done) {
+    const body = { type: 'transfer', origin: '200', amount: 15, destination: '300' }
+
+    chai.request(this.app)
+      .post('/event')
+      .send(body)
+      .end(function (err, res) {
+        expect(err).is.equal(null)
+        expect(res).to.have.status(404)
+        expect(res.body).to.be.equal(0)
         done()
       })
   })
