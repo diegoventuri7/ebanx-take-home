a37d6934bd3fe7e5ba6591dc7613bb616517ac28
Diego Venturi <diego.venturi@objective.com.br>
2021-05-18 21:18:38 -0300

    test: improve api endpoint tests

---
+++ test/unit/transactions-repository.test.js
@@ -0,0 +1,19 @@
+// const chai = require('chai')
+// const transactionsRepository = require('../../src/api/repositories/transactions-repository.js')
+// const expect = chai.expect
+
+// describe('transactions-repository', function () {
+//   it('Happy day: Create new transaction', function (done) {
+//     const transaction = { accountId: '50', type: 'deposit', amount: 103 }
+
+//     transactionsRepository.create(transaction)
+
+//     transactionsRepository.get
+
+
+//     expect(err).is.equal(null)
+//     expect(res).to.have.status(200)
+//     expect(res.body).to.be.equal('pong')
+//     done()
+//   })
+// })
